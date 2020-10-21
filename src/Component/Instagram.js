31d92ba80e15eedb87fda919c493d2557c18bd73
evereichmann <eve.reichmann@gmail.com>
2020-10-21 14:45:48 -0500

    small style changes

--- src/Component/Instagram.js
+++ src/Component/Instagram.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Instgram = () => {
     return ( 
-        <div style={{border: "2px solid black", marginTop: "5px", padding: "5px"}}>
+        <div style={{border: "2px solid black", marginTop: "5px", padding: "5px", width: "49%"}}>
             <h3>Instgram Images Widget</h3>
             <div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}} ></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}} ></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
             </div>
             <div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
             </div>
             <div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
-            <div style={{border: "2px solid black", padding: "2%", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
+            <div style={{border: "2px solid black", padding: "60px", display: "inline-block", verticalAlign: "middle", margin: "5px"}}></div>
             </div>
         </div>
      );
