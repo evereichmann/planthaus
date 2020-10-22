4926a82f191c7e937f15c3e5fc8d4a7c083b8669
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-10-22 10:47:39 -0500

    firebase hooked in, db rules set

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom"
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "planthaus-13436.firebaseapp.com",
+  databaseURL: "https://planthaus-13436.firebaseio.com",
+  projectId: "planthaus-13436",
+  storageBucket: "planthaus-13436.appspot.com",
+  messagingSenderId: "649876866393",
+  appId: "1:649876866393:web:591f532df8e938a4f6f848",
+  measurementId: "G-0MTGDEL1FK"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
